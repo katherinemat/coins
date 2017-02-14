e0b317cf8904ee7fa1b61d9e7a8c4784e78fd822
Grinil Khanna and Kaz Matthews <grinil+kaz@>
2017-02-14 15:35:41 -0800

    add code to display information on kestrel server.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,11 +1,22 @@
-// using Nancy;
-// using System.COllections.Generic;
-// using Coins;
-//
-// namespace NancyModule: HomeModule
-// {
-//   public class HomeModule()
-//   {
-//     Get["/"] {}
-//   }
-// }
+using Nancy;
+using System.Collections.Generic;
+using System;
+
+namespace Coins
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+      Post["/"] = _ => {
+        int userInput = Request.Form["number"];
+        Coins newCoins = new Coins(userInput);
+        List<string> change = newCoins.Change();
+        return View["index.cshtml", change];
+      };
+    }
+  }
+}
